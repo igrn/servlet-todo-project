5a1dfbc5fc4228708be4b6261e19e81d406cf682
igrn <iazelenin@icloud.com>
2021-01-11 17:51:36 +0400

    minor bugfix

--- src/main/java/igrn/todo/Printable.java
+++ src/main/java/igrn/todo/Printable.java
@@ -4,9 +4,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+//TODO: переделать в абстрактный сервлет и от него наследовать column и ticket servlets??
 public interface Printable {
     default void printResponse(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        if (request.getQueryString().isEmpty()) {
+        if (request.getQueryString() == null) {
             printList(response);
         } else {
             try {
