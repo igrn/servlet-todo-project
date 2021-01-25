eb676bbeff41e91066230effb4e16abab9b25682
igrn <iazelenin@icloud.com>
2021-01-26 00:25:41 +0400

    added taskboard tests

--- src/test/java/igrn/todo/board/ColumnTest.java
+++ src/test/java/igrn/todo/board/ColumnTest.java
@@ -1,61 +1,39 @@
 package igrn.todo.board;
 
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class ColumnTest {
+class ColumnTest extends TicketTest {
     static List<Column> columns;
-    static List<List<Ticket>> tickets;
-    static int idCount = 0;
 
     @BeforeAll
     static void setUp() {
-        tickets = new ArrayList<>() {{
-            add(createTickets(3));
-            add(createTickets(1));
-            add(createTickets(2));
-        }};
+        TicketTest.setUp();
         columns = new ArrayList<>() {{
-            add(new Column(0, "Column 0", tickets.get(0)));
-            add(new Column(1, "Column 1", tickets.get(1)));
-            add(new Column(2, "Column 2", tickets.get(2)));
+            add(new Column(0, "Column 0", tickets));
+            add(new Column(1, "Column 1", tickets));
+            add(new Column(2, "Column 2", tickets));
         }};
-    }
-
-    // Метод проверен дебагом; создает список тикетов для одной колонки
-    protected static List<Ticket> createTickets(int size) {
-        List<Ticket> tickets = new ArrayList<>();
-        for (int i = 0; i < size; i++) {
-            int id = idCount + i;
-            String title = "Ticket " + id;
-            tickets.add(new Ticket(id, title));
-        }
-        idCount += size;
-        return tickets;
     }
 
     @AfterAll
     static void tearDown() {
+        TicketTest.tearDown();
         columns = null;
-        tickets = null;
-        idCount = 0;
     }
 
-    @Test
+    @Test @Override
     void testFind() {
-        Column expected = columns.get(2);
-        Column actual = Column.find(2, columns);
+        Column expected = columns.get(1);
+        Column actual = Column.find(1, columns);
         assertEquals(expected, actual);
     }
 
-    @Test
+    @Test @Override
     void testFindIfIdNotFound() {
         assertThrows(RuntimeException.class, () -> Column.find(-1, columns));
     }
--- src/test/java/igrn/todo/board/TaskboardTest.java
+++ src/test/java/igrn/todo/board/TaskboardTest.java
@@ -2,26 +2,48 @@ package igrn.todo.board;
 
 import org.junit.jupiter.api.*;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 class TaskboardTest extends ColumnTest {
-    static Taskboard taskboard;
-    static List<Ticket> flatTickets;
+    static List<Taskboard> taskboards;
 
     @BeforeAll
     static void setUp() {
         ColumnTest.setUp();
-        taskboard = new Taskboard(0, "Taskboard 0", columns);
+        taskboards = new ArrayList<>() {{
+            add(new Taskboard(0, "Taskboard 0", columns));
+            add(new Taskboard(1, "Taskboard 1", columns));
+            add(new Taskboard(2, "Taskboard 2", columns));
+        }};
     }
 
     @AfterAll
     static void tearDown() {
         ColumnTest.tearDown();
-        taskboard = null;
+        taskboards = null;
+    }
+
+    @Test @Override
+    void testFind() {
+        Taskboard expected = taskboards.get(1);
+        Taskboard actual = Taskboard.find(1, taskboards);
+        assertEquals(expected, actual);
+    }
+
+    @Test @Override
+    void testFindIfIdNotFound() {
+        assertThrows(RuntimeException.class, () -> Taskboard.find(-1, taskboards));
     }
 
     @Test
     void testGetAllTickets() {
-
+        List<Ticket> expected = columns.stream().flatMap(column -> column.getTickets().stream())
+                                                .collect(Collectors.toList());
+        List<Ticket> actual = taskboards.get(1).getAllTickets();
+        assertEquals(expected, actual);
     }
 }
